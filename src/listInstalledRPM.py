84d46fee04f4240579a6babe58b487bfb17187bb
Mohammed El-Afifi <Mohammed_ElAfifi@yahoo.com>
2014-04-11 08:14:54 +0200

    updating the copyright header notice in all files

--- src/listInstalledRPM.py
+++ src/listInstalledRPM.py
@@ -9,7 +9,7 @@ Usage: listInstalledRPM.py LOGFILE
 
 ############################################################
 #
-# Copyright 2009, 2010, 2011, 2012, 2013 Mohammed El-Afifi
+# Copyright 2009, 2010, 2011, 2012, 2013, 2014 Mohammed El-Afifi
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as
